24a7f6d7121eb94abc0677b94b5d8bb056989d34
rimzoni <rimzoni@gmail.com>
2016-01-13 16:47:01 +0100

    Modified s tore places

--- app/sources/PlaceSource.js
+++ app/sources/PlaceSource.js
@@ -1,12 +1,12 @@
 var PlaceActions = require('../actions/PlaceActions');
 
 var mockData = [
-  { id: 0, name: 'Druga kuca', description: 'Odlican steak u lepini!', image: 'http://placehold.it/150x100',lat: '43.8539973',lng: '18.3860037', category:'Food' , active: true },
-  { id: 1, name: 'Slatko i Slano', description: 'Super kafa', image: 'http://placehold.it/150x100',lat: '43.8530275',lng: '18.3834648', category:'Bar' , active: true},
-  { id: 2, name: 'Paper Moon', description: 'Pice za 10', image: 'http://placehold.it/150x100',lat: '43.8508628',lng: '18.38866', category:'Club', active: true },
-  { id: 3, name: 'Buenos Aires', description: 'test1', image: 'http://placehold.it/150x100',lat: '43.8539973',lng: '18.3860037', category:'Food' , active: true},
-  { id: 4, name: 'Cairo', description: 'test2', image: 'http://placehold.it/150x100',lat: '43.8539973',lng: '18.3860037', category:'Bar' , active: true},
-  { id: 5, name: 'Chicago', description: 'test3', image: 'http://placehold.it/150x100',lat: '43.8539973',lng: '18.3860037', category:'Club', active: true }
+  { id: 0, name: 'Druga kuca', description: 'Druga kuća - Lora je nastala iz želje da svojim prijateljima', image: 'http://placehold.it/150x100',lat: '43.8539973',lng: '18.3860037', category:'Food' , active: true },
+  { id: 1, name: 'Slatko i Slano', description: 'Veoma dobra kuhinja.', image: 'http://placehold.it/150x100',lat: '43.8530275',lng: '18.3834648', category:'Bar' , active: true},
+  { id: 2, name: 'Paper Moon', description: 'Restoran/Vinarija', image: 'http://placehold.it/150x100',lat: '43.8508628',lng: '18.38866', category:'Club', active: true },
+  { id: 3, name: 'Buenos Aires', description: 'Juzno Americka kuhinja', image: 'http://placehold.it/150x100',lat: '43.8539973',lng: '18.3860037', category:'Food' , active: true},
+  { id: 4, name: 'Cairo', description: 'Very prestious bar.', image: 'http://placehold.it/150x100',lat: '43.8539973',lng: '18.3860037', category:'Bar' , active: true},
+  { id: 5, name: 'Chicago', description: 'Club in the middle of the city', image: 'http://placehold.it/150x100',lat: '43.8539973',lng: '18.3860037', category:'Club', active: true }
 ];
 
 var PlaceSource = {
